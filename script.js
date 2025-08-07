fc652bb556294455afe8394bb54ca84829606cfb
Plab0m <nseipablo@gmail.com>
2025-08-07 19:19:09 -0300

    implementado "do while" - estudo

--- script.js
+++ script.js
@@ -1,33 +1,37 @@
 const ContactList = [
-  { name: "Alice", phone: "123-456-7890" },
-  { name: "Bob", phone: "987-654-3210" },
-  { name: "Charlie", phone: "555-555-5555" },
-  { name: "Diana", phone: "444-444-4444" },
-  { name: "Eve", phone: "333-333-3333" },
-  { name: "Frank", phone: "222-222-2222" },
+    { name: "Alice", phone: "123-456-7890" },
+    { name: "Bob", phone: "987-654-3210" },
+    { name: "Charlie", phone: "555-555-5555" },
+    { name: "Diana", phone: "444-444-4444" },
+    { name: "Eve", phone: "333-333-3333" },
+    { name: "Frank", phone: "222-222-2222" },
 ];
 
 const BtnPesq = document.getElementById("searchButton");
-
 BtnPesq.addEventListener("click", function () {
-  const input = document
-    .getElementById("searchInput")
-    .value.trim()
-    .toLowerCase();
-  const spanPesq = document.getElementById("spanPesq");
-  const ResultPesq = document.getElementById("resultadoPesquisa");
-  const visibilityResult = document.getElementById("containerInputsResult");
+    const input = document.getElementById("searchInput").value.trim().toLowerCase();
+    const spanPesq = document.getElementById("spanPesq");
+    const ResultPesq = document.getElementById("resultadoPesquisa");
+    const visibilityResult = document.getElementById("containerInputsResult");
+    let i = 0;
+    let foundContact = null;
 
-  for (const contact of ContactList) {
-    if (contact.name.toLowerCase().trim() === input) {
-      spanPesq.innerHTML = "Contato Encontrado:";
-      ResultPesq.innerHTML = `${contact.name} - ${contact.phone}`;
-      break;
+    if (ContactList.length > 0) {
+
+        do {
+            const contact = ContactList[i];
+            if (contact.name.toLowerCase() === input) {
+                foundContact = contact
+            }
+            i++;
+        } while (foundContact === null && i < ContactList.length);
+    }
+    visibilityResult.style.visibility = "visible";
+    if (foundContact){
+      spanPesq.innerHTML = "Contatos Encontrados:";
+      ResultPesq.innerHTML = foundContact.name + " - " + foundContact.phone;
     } else {
       spanPesq.innerHTML = "Nenhum Contato Encontrado.";
       ResultPesq.innerHTML = "";
     }
-  }
-
-  visibilityResult.style.visibility = "visible";
 });
